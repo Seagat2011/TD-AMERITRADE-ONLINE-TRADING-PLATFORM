5d275141491fbd5ab1c27038ca4e5a65130dbc7f
unknown <Seagat2011@github.com>
2019-02-15 15:29:22 -0500

    Added 11-13 APIs

---
+++ 2019/js/market.js
@@ -0,0 +1,57 @@
+/*
+
+AUTHOR: Seagat2011
+
+DESCRIPTION:
+
+*/
+
+class __market {
+	constructor(o){
+	}
+	GetMovers(o){
+	}
+	GetOptionsChain(o){
+	}
+	GetPriceHistory(o){
+	}
+	GetQuote(o){
+	}
+	GetQuotes(o){
+	}
+	GetTransaction(o){
+	}
+	GetTransactions(o){
+	}
+	GetPreferences(o){
+	}
+	UpdatePreferences(o){
+	}
+	GetStreamerSubscriptionKeys(o){
+	}
+	GetUserPrincipals(o){
+	}
+	PlaceOrder(o){
+	}
+	CancelOrder(o){
+	}
+	GetOrder(o){
+	}
+	GetOrdersByPath(o){
+	}
+	GetOrdersByQuery(o){
+	}
+	PostAccessToken(o){
+	}
+	SearchInstruments(o){
+	}
+	GetInstruments(o){
+	}
+	GetHoursForMultipleMarkets(o){
+	}
+	GetHoursForSingleMarket(o){
+	}
+	CreateWatchlist(o){
+	}	
+}
+market = new __market
