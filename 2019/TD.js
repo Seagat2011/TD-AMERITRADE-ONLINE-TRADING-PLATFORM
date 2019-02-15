5eec6357a5230dcad6cc21cb3f5e9c4f35469d01
unknown <Seagat2011@github.com>
2019-02-15 14:56:24 -0500

    New repository

---
+++ 2019/TD.js
@@ -0,0 +1,14 @@
+// ------------------- Startup ------------------- //
+// get ETF-positions //
+// get EQUITY-positions //
+// rebalance positions //
+// ------------------- Trade online ------------------- //
+// Load Options Exchange //
+// get movers (to avoid them!) //
+// P/L DAY (Percent)
+// BALANCE:
+
+function word (){
+
+
+}
